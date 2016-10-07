d70d1f7a05b1e61b27c89a0914be2ecb06bed6a2
Meutraa <paul@lost.host>
2016-10-07 21:51:44 +0200

    Cutting down on code.

--- main.go
+++ main.go
@@ -10,44 +10,39 @@ import (
 	"net/http"
 )
 
-type Login struct {
-	Type     string `json:"type"`
-	User     string `json:"user"`
-	Password string `json:"password"`
-}
-
 var session Session
+var host, user, pass string
 
 type Session struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 	DeviceId     string `json:"device_id"`
-	TokenId      int
+}
+
+func apistr(str string) string {
+	return host + "/_matrix/client/r0/" + str + "access_token=" + session.AccessToken
 }
 
 func main() {
-	var host, user, pass string
 	flag.StringVar(&host, "host", "", "matrix homeserver")
 	flag.StringVar(&user, "user", "", "username")
 	flag.StringVar(&pass, "pass", "", "password")
 	flag.Parse()
 
-	session = login(host, user, pass)
-	fmt.Printf("%s\n", session.AccessToken)
-	sync(host)
-
-	logout(host)
+	login()
+	sync()
+	logout()
 }
 
-func sync(host string) {
-	res, _ := http.Get(host + "/_matrix/client/r0/sync" + "?access_token=" + session.AccessToken)
+func sync() {
+	res, _ := http.Get(apistr("sync?"))
 	body, _ := ioutil.ReadAll(res.Body)
 	fmt.Printf("%s\n", body)
 	res.Body.Close()
 }
 
-func logout(host string) {
-	res, _ := http.Post(host+"/_matrix/client/r0/logout"+"?access_token="+session.AccessToken, "application/json", nil)
+func logout() {
+	res, _ := http.Post(apistr("logout?"), "application/json", nil)
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
 		body, _ := ioutil.ReadAll(res.Body)
@@ -55,12 +50,11 @@ func logout(host string) {
 	}
 }
 
-func login(host string, user string, pass string) Session {
-	b, _ := json.Marshal(Login{"m.login.password", user, pass})
-
-	res, err := http.Post(host+"/_matrix/client/r0/login", "application/json", bytes.NewBuffer(b))
+func login() {
+	res, err := http.Post(host+"/_matrix/client/r0/login", "application/json",
+		bytes.NewBuffer([]byte("{\"type\":\"m.login.password\",\"user\":\""+user+"\",\"password\":\""+pass+"\"}")))
 	if err != nil || res.StatusCode != 200 {
-		log.Fatalf("Login request to server failed: %s, %s\n", err, http.StatusText(res.StatusCode))
+		log.Fatalf("Login failed: %s, %s\n", err, http.StatusText(res.StatusCode))
 	}
 
 	fmt.Println("The user has been authenticated")
@@ -68,10 +62,7 @@ func login(host string, user string, pass string) Session {
 	body, _ := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
 
-	var ses Session
-	err = json.Unmarshal(body, &ses)
-	if err != nil || ses.AccessToken == "" {
+	if json.Unmarshal(body, &session) != nil || session.AccessToken == "" {
 		log.Fatalf("Login response could not be parsed: %s,%s\n", err, body)
 	}
-	return ses
 }
